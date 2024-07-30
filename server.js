a6233ef599a859a56615fdca3e0b81e054fdaff1
NagiNatsuki <88655239+IcsShark@users.noreply.github.com>
2024-07-30 15:29:15 +0800

    room server

--- server.js
+++ server.js
@@ -10,26 +10,134 @@ app.use(express.static(path.join(__dirname, 'src')));
 const server = http.createServer(app); // http server
 const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 }); // socket.io
 
-var onlinePlayers = 0;
+let rooms = {};
+let players = {};
 
 io.on('connect', (socket) => {
-    onlinePlayers += 1;
-    console.log('A user connected, total player numbers: ' + onlinePlayers);
+    socket.on('joinRoom', (roomId, name) => {
+        const player = {name: name, role: "Spec", Id: socket.id};
+        if (!rooms[roomId]) {
+            rooms[roomId] = [0, 0, 1, false];
+            players[roomId] = [];
+        }
+        else{
+            rooms[roomId][2] += 1;
+        }
+        players[roomId].push(player);
+        
+        socket.join(roomId);
+        console.log(`User joined room: mission#${roomId}`);
+        io.to(roomId).emit("updatePlayer", players[roomId]);
+    })
 
+    function changeRole(roomId, name, newRole) {
+        let player = players[roomId].find(player => player.name === name);
+        if (player) {
+            player.role = newRole;
+        }
+    }
+
+    socket.on('P1', (roomId, name, role) =>{
+        if(rooms[roomId][3]){
+            return;
+        }
+        if (rooms[roomId] && rooms[roomId][0] == 0){
+
+            io.to(roomId).emit("P1");
+            rooms[roomId][0] += 1;
+
+            changeRole(roomId, name, "P1");
+
+            if(role == "P2"){
+                rooms[roomId][1] -= 1;
+            }else{
+                rooms[roomId][2] -= 1;
+            }
+            console.log(rooms[roomId]);
+            io.to(roomId).emit("updatePlayer", players[roomId]);
+        }
+    })
+    socket.on('P2', (roomId, name, role) =>{
+        if(rooms[roomId][3]){
+            return;
+        }
+        if (rooms[roomId] && rooms[roomId][1] == 0){
+            io.to(roomId).emit("P2");
+            rooms[roomId][1] += 1;
+            
+            changeRole(roomId, name, "P2");
+
+            if(role == "P1"){
+                rooms[roomId][0] -= 1;
+            }else{
+                rooms[roomId][2] -= 1;
+            }
+            console.log(rooms[roomId]);
+            io.to(roomId).emit("updatePlayer", players[roomId]);
+        }
+    })
+    socket.on('Spec', (roomId, name, role) =>{
+        if(rooms[roomId][3]){
+            return;
+        }
+        if(role !== "Spec"){
+            io.to(roomId).emit("Spec");
+            rooms[roomId][2] += 1;
+    
+            changeRole(roomId, name, "Spec");
+    
+            if(role == "P1"){
+                rooms[roomId][0] -= 1;
+            }else{
+                rooms[roomId][1] -= 1;
+            }
+            console.log(rooms[roomId]);
+            io.to(roomId).emit("updatePlayer", players[roomId]);
+        }
+    })
     socket.on('disconnect', () => {
-        onlinePlayers -= 1;
-        console.log('A user disconnected, total player numbers: ' + onlinePlayers);
+        for (const roomId in players) {
+            const playerIndex = players[roomId].findIndex(player => player.Id === socket.id);
+            if(playerIndex !== -1){
+                const player = players[roomId][playerIndex];
+                if (player.role === "P1") {
+                    rooms[roomId][0] -= 1;
+                } else if (player.role === "P2") {
+                    rooms[roomId][1] -= 1;
+                } else {
+                    rooms[roomId][2] -= 1;
+                }
+                players[roomId].splice(playerIndex, 1);
+                break;
+            }
+        }
+    });
+
+    socket.on("GameStart", (roomId) => {
+        rooms[roomId][3] = true;
+        socket.to(roomId).emit("GameLock", true);
     });
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'cover.html'));
+})
+
+app.get('/menu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'menu.html'));
+});
+
+app.get(`/room/:roomid`, (req, res) => {
+    const roomId = req.params.roomid;
+    if (roomId.trim().length !== 0 && roomId !== null) {
+        res.sendFile(path.join(__dirname,  'src', 'room.html'));
+    } else {
+        res.status(404).send('Room not found');
+    }
+})
 
 server.listen(port, function(){
     console.log('Server is running on port ' + port);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/src/cover.html');
-})
-
-app.get('/menu', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/menu.html'));
-});
+module.exports = { io };
--- src/js/fonted.js
+++ src/js/fonted.js
@@ -1,1 +1,142 @@
 const socket = io();
+
+let gamelock = false;
+
+function SetCookie(cname, cvalue){
+    document.cookie = cname + '=' + cvalue + ';';
+}
+
+function GetCookie(cname){
+    let c = document.cookie;
+    let name = cname + '=';
+    let clist = document.cookie.split(';');
+    for(let i = 0; i < clist.length; i++) {
+        let c = clist[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+// cover
+function saveName(){
+    var playername = document.getElementById("name").value;
+    if (playername.trim().length === 0 || playername === null){
+        playername = "guest";
+    }
+    alert("Welcome to HFL, "+playername);
+    SetCookie("playername", playername);
+}
+
+function getName(){
+    var playername = GetCookie("playername");
+    return playername;
+}
+
+function navigateToMenu(){
+    window.location.href = '/menu';
+}
+// menu
+function saveCode(){
+    var missionCode = document.getElementById("code").value;
+    if (missionCode.trim().length === 0 || missionCode === null){
+        alert("error, please enter the mission code again");
+    }
+    else{
+        //alert("#" + missionCode + " mission start");
+        SetCookie("Room", missionCode);
+        joinRoom();
+    }
+}
+
+function getCode(){
+    return GetCookie("Room");
+}
+
+function joinRoom(){
+    let roomid = getCode();
+    window.location.href = `/room/${roomid}`;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.getElementById('room')) {
+        let roomid = getCode();
+        let playername = getName();
+        SetCookie("role","Spec");
+        socket.emit('joinRoom', roomid, playername);
+        console.log("room loded");
+    }
+});
+//room
+let newPlayerLock = true;
+
+socket.on("updatePlayer", (players) => {
+    const classNames = ["P1_list", "P2_list", "Spec_list"];
+    classNames.forEach(className => {
+        const elements = document.getElementsByClassName(className);
+        elements[0].innerHTML = '';
+    });
+    players.forEach(player => {
+        if(player.role === "P1"){
+            element = document.getElementsByClassName("P1_list");
+        }else if(player.role === "P2"){
+            element = document.getElementsByClassName("P2_list");
+        }else{
+            element = document.getElementsByClassName("Spec_list");
+        }
+        element[0].insertAdjacentHTML("beforeend", `<div class="spec_nametag">${player.name}</div>`);
+    });
+    newPlayerLock = false;
+});
+
+function becomeP1(){
+    let missionCode = getCode();
+    let playername = getName();
+    let role = GetCookie("role");
+    if((role == "P2" || role == "Spec")){
+        socket.emit("P1",missionCode , playername, role);
+        socket.on("P1", () =>{
+            SetCookie("role","P1");
+        })
+    }
+}
+
+function becomeP2(){
+    let missionCode = getCode();
+    let playername = getName();
+    let role = GetCookie("role");
+    if((role == "P1" || role == "Spec")){
+        socket.emit("P2",missionCode , playername, role);
+        socket.on("P2", () =>{
+            SetCookie("role","P2");
+        })
+    }
+}
+
+function becomeSpec(){
+    let missionCode = getCode();
+    let playername = getName();
+    let role = GetCookie("role");
+    if(role != "Spec"){
+        socket.emit("Spec",missionCode , playername, role);
+        socket.on("Spec", () =>{
+            SetCookie("role","Spec");
+        });
+    }
+}
+
+function back(){
+    navigateToMenu();
+    SetCookie("Room", '');
+}
+
+function next(){
+    let missionCode = getCode();
+    socket.emit("GameStart", missionCode);
+    socket.on("GameLock", (lock) => {
+        gamelock = lock;
+    });
+}
